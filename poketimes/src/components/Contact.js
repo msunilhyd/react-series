0158c76d0c049154eb10b2c9f911ed08e65c5e1e
msunilhyd <msunilhyd@gmail.com>
2019-06-10 05:21:59 -0700

    10-Jun-19 : Added the navbar and router for react

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center"> Contact </h4>
+            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa dolorem impedit dolore corrupti, eius ex quo explicabo odit nisi, quidem nulla? Non velit voluptatem dolor magnam esse et impedit ex?</p>
+        </div>
+    )
+}
+
+
+export default Contact
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center"> Home </h4>
+            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa dolorem impedit dolore corrupti, eius ex quo explicabo odit nisi, quidem nulla? Non velit voluptatem dolor magnam esse et impedit ex?</p>
+        </div>
+    )
+}
+
+
+export default Home
