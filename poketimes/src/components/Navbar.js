dff916e8e14111020a86ed28b604ee0cd6d2cc8f
msunilhyd <msunilhyd@gmail.com>
2019-06-11 03:58:27 -0700

    11-Jun-19 : Added the axios to fetch rest data

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,11 +1,7 @@
 import React from 'react'
-import { Link, NavLink, withRouter } from 'react-router-dom'
+import { NavLink, withRouter } from 'react-router-dom'
 
 const Navbar = (props) => {
-
-    setTimeout(() => {
-        props.history.push('/about')
-    }, 5000);
 
     return (
         <nav className="nav-wrapper blue darken-3">
