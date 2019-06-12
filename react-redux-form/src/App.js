d9f106dbb73f6c6ab6e9835696c8a60394aaf95a
msunilhyd <msunilhyd@gmail.com>
2019-06-12 02:36:19 -0700

    12-Jun-19 : Created the new project for redux practice and added the necessary react code

---
+++ react-redux-form/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+import Posts from './components/Posts';
+import Postform from './components/Postform';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Postform />
+        <hr />
+        <Posts />
+      </header>
+    </div>
+  );
+}
+
+export default App;
