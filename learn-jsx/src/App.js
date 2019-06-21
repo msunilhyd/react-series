2be1534ba91ae8521157f795f1283c63ceff9db9
msunilhyd <msunilhyd@gmail.com>
2019-06-21 03:51:55 -0700

    21-Jun-19 : Added the sayHello method that returns the html instead of the hard coded html

--- learn-jsx/src/App.js
+++ learn-jsx/src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function sayHello() {
+  return <h1> Hello Linus</h1>
+}
+
 function App() {
   return (
-    <div className={1 + 1} id="name"> Hello </div>
+    sayHello()
   );
 }
 
